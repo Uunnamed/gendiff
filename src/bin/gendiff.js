b599d3334ba97efb55ca43d3fef33216eacf01ec
Uunnamed <Uunnamed@mail.ru>
2017-01-23 16:12:23 +0300

    Step 1 fix bags eslint

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,7 +6,7 @@ import program from 'commander';
 
 program
   .command('gendiff <first_config> <second_config>')
-  .version('0.0.1')
+  .version('0.0.2')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
   .parse(process.argv);
