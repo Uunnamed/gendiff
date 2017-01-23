f7b4991c90bda66e434f20f79d4da54bd074b807
Uunnamed <Uunnamed@mail.ru>
2017-01-23 18:49:04 +0300

    Step 1 fix commander config

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,9 +5,8 @@ import program from 'commander';
 
 
 program
-  .usage('[options] <first_config> <second_config>')
-  .arguments('<first_config> <second_config>')
   .version('0.0.3')
+  .arguments('<first_config> <second_config>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
   .parse(process.argv);
