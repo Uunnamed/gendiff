f8e044fbf5bb254abbb90d48239cf2d78c756b5a
Uunnamed <Uunnamed@mail.ru>
2017-01-26 20:49:08 +0300

    Step 4 add ini parser

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -17,3 +17,10 @@ test('getDiffYAML', () => {
   expect(getDiff(yaml1, yaml2))
   .toBe('{\n  host: hexlet.io\n+ timeout: 20\n- timeout: 50\n- proxy: 123.234.53.22\n+ verbose: true\n}');
 });
+
+const ini1 = path.resolve(__dirname, '__fixtures__/before.ini');
+const ini2 = path.resolve(__dirname, '__fixtures__/after.ini');
+test('getDiffINI', () => {
+  expect(getDiff(ini1, ini2))
+  .toBe('{\n  host: hexlet.io\n+ timeout: 20\n- timeout: 50\n- proxy: 123.234.53.22\n+ verbose: true\n}');
+});
--- src/getparser.js
+++ src/getparser.js
@@ -1,13 +1,16 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const parsers = new Map();
 
 const jsonParser = file => JSON.parse(file);
 const yamlParser = file => yaml.safeLoad(file);
+const iniParser = file => ini.parse(file);
 
 const setParsers = () => {
   parsers.set('json', jsonParser);
   parsers.set('yaml', yamlParser);
+  parsers.set('ini', iniParser);
 };
 
 const getParser = (type) => {
