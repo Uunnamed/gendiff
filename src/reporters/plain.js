b5d6ec5ea17f0a21a39ce8f882b9443ad12b0339
Shipilov Aleksei <alexkontext@yandex.ru>
2019-11-30 22:22:03 +0300

    some refactoring

--- src/reporters/plain.js
+++ src/reporters/plain.js
@@ -1,22 +1,14 @@
-// @flow
-
 const genPlainEnd = (arrDiffObj, key = '') => arrDiffObj.map(({
   name, status, value, oldValue, children,
 }) => {
   const parentKey = key === '' ? '' : `${key}.`;
-  if (status === 'object') {
-    return `${parentKey}${genPlainEnd(children, name)}`;
+  switch (status) {
+    case 'object': return `${parentKey}${genPlainEnd(children, name)}`;
+    case 'updated': return `${parentKey}${name}' was updated. From '${oldValue}' to '${value}'`;
+    case 'removed': return `${parentKey}${name}' was removed`;
+    case 'added': return `${parentKey}${name}' was added with ${typeof value === 'object' ? 'complex value' : `value: ${value}`}`;
+    default: return '';
   }
-  if (status === 'updated') {
-    return `${parentKey}${name}' was updated. From '${oldValue}' to '${value}'`;
-  }
-  if (status === 'removed') {
-    return `${parentKey}${name}' was removed`;
-  }
-  if (status === 'added') {
-    return `${parentKey}${name}' was added with ${typeof value === 'object' ? 'complex value' : `value: ${value}`}`;
-  }
-  return '';
 }).filter((e) => !!e).join('\n');
 
 export default (arrDiffObj) => genPlainEnd(arrDiffObj, '').split('\n').map((e) => `Property '${e}`).join('\n');
